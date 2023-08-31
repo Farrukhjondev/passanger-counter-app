20dd5b1f76ef5c8f5e71434de434133de22b19f9
Farrukhjon S <farrukhjon.dev@gmail.com>
2023-08-31 10:25:39 +0500

    add save btn

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // document.getElementById('count').innerText = 5;
 // // new variable
+let saveEl=document.getElementById('save-el')
 let countEl=document.getElementById('count-el')
 let count = 0;
+
+
+
 function increment() {
   count += 1;
   countEl.innerText = count;
+} 
+
+function save() {
+  let counteStr = ' '+ count + ' - ';
+  saveEl.innerText += counteStr
   console.log(count)
 }
